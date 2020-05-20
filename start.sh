532d058161d3b1d5a16a2f628bd98597e2723d4d
Vinicius Feitosa da Silva <viniciusfesil@gmail.com>
2020-05-20 11:32:54 -0300

    🎨 Improve structure
    Co-authored-by: Samuel Felix <sfelix_martins@hotmail.com>
    Co-authored-by: Lucas Mendes <lucasffm@gmail.com>
    Co-authored-by: Camilo Fernandes <camilo.analista@gmail.com>

--- start.sh
+++ start.sh
@@ -1,7 +1,18 @@
 #!/usr/bin/env bash
 
-source config.sh
+gitRemoteURL=$(git config --get remote.origin.url)
+scriptDirectory=$(dirname $0)
+configuration="${scriptDirectory}/config.sh"
+source $configuration
 
-curl -H "Content-Type: application/json" \
--X POST \
--d "{\"username\": \"${username}\", \"content\": \"${message}\"}" $url
+if [ $gitRemoteURL = $myRepositoryURL ];
+then
+
+  lastModifiedUser=$(git log --format='%an' -n 1)
+  lastModifiedTitle=$(git log --format='%s' -n 1)
+  message="[Server]\nUser **${lastModifyUser}** sends a commit. \nCommit title: **${lastModifyTitle}**"
+
+  curl -H "Content-Type: application/json" \
+  -X POST \
+  -d "{\"username\": \"${username}\", \"content\": \"${message}\"}" $url
+fi
